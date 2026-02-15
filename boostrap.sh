6c89887539e732afc0005c88f1427b557dbf0ac9
nareshungarala396 <nareshungarala220@gmail.com>
2026-02-15 14:29:29 +0530

    terraform

--- boostrap.sh
+++ boostrap.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+component=$1
+environment=$2
+dnf install ansible -y
+
+
 REPO_URL=https://github.com/daws-86s/ansible-roboshop-roles-tf.git
 REPO_DIR=/opt/roboshop/ansible
 ANSIBLE_DIR=ansible-roboshop-roles-tf
